ec066ebe85f5fa9512cb951b1e83c0a38ce8c7ba
eduardo404 <eduardoop@protonmail.com>
2021-05-08 09:12:57 -0300

    Selecting Columns

---
+++ src/users.js
@@ -0,0 +1,16 @@
+import { List, Datagrid, TextField, EmailField } from "react-admin";
+
+export const UserList = props => {
+  return (
+    <List {...props}>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" />
+        <EmailField source="email" />
+        <TextField source="phone" />
+        <TextField source="website" />
+        <TextField source="company.name" />
+      </Datagrid>
+    </List>
+  );
+};
